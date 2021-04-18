479c5de4980b9ee3a072a9d5392e310c8c5e7d97
KAUSTUBHVVM <78797017+KAUSTUBHVVM@users.noreply.github.com>
2021-04-18 20:35:02 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# LETS-GOOOO
+
---
+++ sketch.js
@@ -0,0 +1,79 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var p1,p2,p3,p4,p5;
+var plinkos = [];
+var divisions =[];
+var divisionHeight=300;
+var score =0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+  //create division objects
+  for (var k = 0; k <=800; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  //create 1st row of plinko objects
+  for (var j = 75; j <=width; j=j+50) { 
+    plinkos.push(new Plinko(j,75));
+  }
+
+  //create 2nd row of plinko objects
+  for (var j = 50; j <=width-10; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,175));
+  }
+
+  //create 3rd row of plinko objects
+  for (var j = 75; j <=width-10; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,275));
+  }
+  
+  //create 4th row of plinko objects
+  for (var j = 50; j <=width-10; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,375));
+  }
+
+  //create particle objects
+  p1 = new particles(100,50,5);
+  p2 = new particles(200,50,5);
+  p3 = new particles(300,50,5);
+  p4 = new particles(400,50,5);
+  p5 = new particles(500,50,5);
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+ 
+  Engine.update(engine);
+  ground.display();
+  
+  //display the plinkos 
+  for (var i = 0; i < plinkos.length; i++) {
+    plinkos[i].display();   
+  }
+   
+  //display the divisions
+  for (var k = 0; k < divisions.length; k++) {
+    divisions[k].display();
+  }
+
+  //display the paricles 
+  p1.display();
+  p2.display();
+  p3.display();
+  p4.display();
+  p5.display();
+}
